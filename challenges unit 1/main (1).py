9812fb6d8478500bb1c22bb1647c5e1c48302feb
Santhiya0427 <144333098+Santhiya0427@users.noreply.github.com>
2023-09-18 22:40:44 +0530

    Update main (1).py

--- challenges unit 1/main (1).py
+++ challenges unit 1/main (1).py
@@ -1,7 +1,7 @@
 #leap year
 #Dont change the above syntax
 if ('year%4==0 and year%100!=0 or year%400==0'):
- 'returleapn True'
+ 'return True'
 else :
   'return False'
 year=int (input("Enter a year")) 
